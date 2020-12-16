6f492fe10ae7580d67cbaa91e89a18af51fcbd54
Mgrdich <mgotm13@gmail.com>
2020-12-16 12:39:46 +0400

    BE adding books Controller + model + validation

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 const {Router} = require('express');
 const users = require("./routes/users");
+const books = require("./routes/books");
 const {isAuth} = require("./middlewares/authentication");
 
-
+1
 const router = Router();
 
 router.use('/users', users);
-router.use(isAuth()); //all the routes should require an Authorization
+router.use('/books',books);
+// router.use(isAuth()); //all the routes should require an Authorization
 
 module.exports = router;
---
+++ src/routes/books.js
@@ -0,0 +1,9 @@
+const {Router} = require('express');
+
+const {isAuth,isAuthorized} = require("../middlewares/authentication");
+
+
+
+const router = Router();
+
+module.exports = router;
