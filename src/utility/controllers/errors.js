2b5523355bbfea43498fea8790f5ff1e8d0b7df7
Mgrdich <mgotm13@gmail.com>
2021-01-17 20:09:19 +0400

    BE: errors reformatting  values

--- src/utility/controllers/errors.js
+++ src/utility/controllers/errors.js
@@ -26,4 +26,35 @@ const errorValidation = function (req) {
     }
 };
 
-module.exports = {errorThrower, errorCatcher, errorFormatter, errorValidation};
+const errorValidationFiles = function (req,array) {
+    const errors = validationResult(req).formatWith(errorFormatter);
+
+    if (!errors.isEmpty()) {
+        let isFileError = -1;
+        let obj = {};
+        let errorArray = errors.errors;
+
+        for (let i = 0; i < errorArray.length; i++) {
+            let item  = errorArray[i];
+            if(item.param === 'files') {
+                isFileError = i;
+                obj = errorArray[i];
+                errorArray.splice(i,1);
+            }
+        }
+
+        if (isFileError !== -1) {
+            for (let i = 0; i < array.length; i++) {
+                let value = req.files[array[i]];
+                 if(!value) {
+                    obj.param =  array[i];
+                    errorArray.push(obj)
+                }
+            }
+        }
+        errorThrower("Validation Failed", 422, errors.mapped());
+    }
+
+}
+
+module.exports = {errorThrower, errorCatcher, errorFormatter, errorValidation,errorValidationFiles};
