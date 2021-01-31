699f93061499e202391036d0d58669aa1380ba1a
Mgrdich <mgotm13@gmail.com>
2021-01-31 20:57:03 +0400

    BE: File upload image fetching finalizing authentication

--- src/routes/file.js
+++ src/routes/file.js
@@ -1,9 +1,10 @@
+const {isAuth} = require("../middlewares/authentication");
 const {readImage,readFile} = require("../controllers/file");
 const {Router} = require('express');
 const router = Router();
 
-router.get('/image/:id', readImage);
+router.get('/image/:id', isAuth(), readImage);
 
-router.get('/book/:id', readFile);
+router.get('/book/:id', isAuth(), readFile);
 
 module.exports = router
