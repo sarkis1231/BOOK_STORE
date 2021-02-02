9226282fddfb043ad135421b55788e979086d986
Mgrdich <mgotm13@gmail.com>
2021-02-03 00:10:48 +0400

    BE: minor code refactor validations

--- src/utility/validations.js
+++ src/utility/validations.js
@@ -1,25 +1,72 @@
+const {Books} = require("../models/Books");
+const {Genres} = require("../models/Genre");
 const {MESSAGES} = require("./constants");
 const {Fn} = require("./functions");
+const {Authors} = require("../models/Author");
+
 let ValidationsFns = {};
 
-// TODO move it here the Common rest
-
 ValidationsFns.isItDate = function (value) {
-    if(!Fn.isValidDate(value)){
+    if (!Fn.isValidDate(value)) {
         throw new Error(MESSAGES.NOT_VALID_DATE);
     }
 };
 
-// TODO is Author found
+ValidationsFns.AuthorNameExists = function (value) {
+    return Authors.findOne({name: value}).then(function (author) {
+        if (author) {
+            return Promise.reject(MESSAGES.NAME_ALREADY_EXIST);
+        }
+    });
+};
 
-// TODO Author is not found
+ValidationsFns.AuthorExists = function (value) {
+    if (!Fn.isMongooseValidId(value)) {
+        throw new Error(MESSAGES.INVALID_ID);
+    }
+    return Authors.findById(value).then(function (author) {
+        if (!author) {
+            return Promise.reject(MESSAGES.AUTHOR_IS_NOT_FOUND);
+        }
+    });
+};
 
-// TODO Book is found
+ValidationsFns.GenreExist = function (value) {
+    if (!Fn.isMongooseValidId(value)) {
+        throw new Error(MESSAGES.INVALID_ID);
+    }
+    return Genres.findById(value).then(function (genre) {
+        if (!genre) {
+            return Promise.reject(MESSAGES.GENRE_NOT_FOUND);
+        }
+    });
+};
 
-// TODO Book is not found
+ValidationsFns.GenreNameExists = function (value) {
+    return Genres.findOne({name: value}).then(function (genre) {
+        if (genre) {
+            return Promise.reject(MESSAGES.GENRE_NAME_ALREADY_EXIST);
+        }
+    });
+};
 
-// TODO User Found
+ValidationsFns.BookNameExists = function (value) {
+    return Books.findOne({name: value}).then(function (book) {
+        if (book) {
+            return Promise.reject(MESSAGES.BOOK_NAME_ALREADY_EXIST);
+        }
+    });
+};
 
-// User not Found
+ValidationsFns.BookExists = function (value) {
+    if (!Fn.isMongooseValidId(value)) {
+        throw new Error(MESSAGES.INVALID_QUERY_PARAM);
+    }
+    return Books.findById(value).then(function (book) {
+        if (!book) {
+            return Promise.reject(MESSAGES.BOOK_NOT_FOUND);
+        }
+    });
+};
 
 module.exports = ValidationsFns;
