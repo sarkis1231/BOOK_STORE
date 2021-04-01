c569fef8a35f577c26ce6cf6d711ae01953628a2
Mgrdich <mgotm13@gmail.com>
2021-04-01 22:20:53 +0400

    BE: Books Filters Permission get a specific one

--- src/routes/permissions.js
+++ src/routes/permissions.js
@@ -7,6 +7,8 @@ const {isAuth, isAdmin} = require("../middlewares/authentication");
 
 const router = Router();
 
-router.get("/", isAuth(), isAdmin(), PermissionCtrl.getPermission);
+router.get("/", isAuth(), isAdmin(), PermissionCtrl.getPermissions);
+
+router.get("/:id", isAuth(), isAdmin(), PermissionCtrl.getPermission);
 
 module.exports = router;
