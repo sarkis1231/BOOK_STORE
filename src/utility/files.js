6cfa8ec0b015206e1eb25f094ef1d6847e67ab59
Mgo <mgotm13@gmail.com>
2021-10-18 23:45:13 +0400

    BE: creating directory if it is not found

--- src/utility/files.js
+++ src/utility/files.js
@@ -3,7 +3,6 @@ const path = require("path");
 const util = require('util');
 const fs = require('fs');
 
-const fsAccessPromise = util.promisify(fs.access);
 const fsMkdirPromise = util.promisify(fs.mkdir);
 
 
