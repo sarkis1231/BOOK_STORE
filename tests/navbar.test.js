ec4a1c20241e9dd101938c8dc5a138881beabaa7
Mgo <mgotm13@gmail.com>
2021-10-31 23:18:08 +0400

    Minor testing environment setup

---
+++ tests/navbar.test.js
@@ -0,0 +1,13 @@
+const puppeteer = require('puppeteer');
+
+test('Adds to numbers', () => {
+    const sum = 1 + 2;
+    expect(sum).toEqual(3)
+});
+
+test('We can launch a browser', async () => {
+    const browser = await puppeteer.launch({
+        headless: true
+    });
+    const page = await browser.newPage();
+});
