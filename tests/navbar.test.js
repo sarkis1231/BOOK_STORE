e2e724b49402f169cdbbd05ae9c999e0b8336f05
Mgo <mgotm13@gmail.com>
2022-01-03 19:27:34 +0400

    adding initial tests

--- tests/navbar.test.js
+++ tests/navbar.test.js
@@ -1,4 +1,18 @@
 const puppeteer = require('puppeteer');
+let browser, page = null;
+
+beforeEach(async () => {
+    browser = await puppeteer.launch({
+        headless: true
+    });
+    page = await browser.newPage();
+    // TODO take this from ENV
+    await page.goto('http://localhost:3000');
+});
+
+afterEach(async () => {
+    await browser.close();
+});
 
 test('Adds to numbers', () => {
     const sum = 1 + 2;
@@ -6,9 +20,6 @@ test('Adds to numbers', () => {
 });
 
 test('We can launch a browser', async () => {
-    const browser = await puppeteer.launch({
-        headless: true
-    });
-    const page = await browser.newPage();
-    await page.goto('http://localhost:3000');
+    const text = await page.$eval('h1', el => el.innerHTML);
+    expect(text).toEqual('Welcome to our Book-Library');
 });
