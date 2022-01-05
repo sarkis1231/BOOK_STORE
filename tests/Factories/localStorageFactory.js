f7159656182cd973ecb135d4ce08f51357ea93a1
Mgo <mgotm13@gmail.com>
2022-01-05 16:36:45 +0400

    TEST: Creating User and local storage factories for utility and testing

--- tests/Factories/localStorageFactory.js
+++ tests/Factories/localStorageFactory.js
@@ -1,13 +1,16 @@
 const {sign} = require("jsonwebtoken");
+const JEST_CONSTANTS = require("../utils/constants");
 
 /**
  * @description searches user with admin permission if not one creates it
- * @param userInfo {{
+ * @param payload {{
  *     id:String,
  *     name:String,
  *     role:String
  * }}
+ * @return Promise<String>
  * */
-module.exports = async function localStorageFactory(userInfo) {
-    const payload = userInfo;
+module.exports = async function localStorageFactory(payload) {
+    const token = await sign(payload, JEST_CONSTANTS.SECRET_KEY, {expiresIn: 3600});
+    return `Bearer ${token}`;
 }
