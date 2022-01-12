2949f5c211a93b6b0394241656bddb39811c2d8f
Mgrdich <mgotm13@gmail.com>
2022-01-12 23:14:08 +0400

    TEST: Constants jest modifications

--- tests/utils/constants.js
+++ tests/utils/constants.js
@@ -13,7 +13,7 @@
 // bear in mind node have to be started for this for the process to be evaluated.
 JEST_CONSTANTS = {
     MONGODB_URI: `mongodb://localhost:${process.env.MONGODB_PORT}`,
-    CLIENT_URL: process.env.CLIENT_URL,
+    CLIENT_URL: process.env.CLIENT_URL, // TODO delete after the proxy
     BE_BASE_URL: `http://localhost:${process.env.PORT}`,
     SECRET_KEY: process.env.SECRET_KEY,
     USER_TEST_DEFAULT_NAME: process.env.USER_TEST_DEFAULT_NAME,
