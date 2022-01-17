e83571ddcc1bedba7495b0e939dcc0f2ec27d2a7
Mgrdich <mgotm13@gmail.com>
2022-01-17 21:06:59 +0400

    FE: React FE Code modifications adding error page

--- client/src/components/ErrorPage.jsx
+++ client/src/components/ErrorPage.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import styled from "styled-components";
+import {useTranslation} from "react-i18next";
+import Button from "./Reusable/Button";
+import {FlexContainer} from "../styled/layout.styled";
+
+
+const ErrorPageNumber = styled.div`
+  text-align: center;
+  font-size: 120px; 
+  color: ${({theme}) => theme.secondary_text};
+  margin: 10px 0;
+`;
+
+const ErrorPageText = styled.div`
+  text-align: center;
+  font-size: 18px;
+  color: white;
+  margin: 0 0 10px 0;
+`;
 
 const ErrorPage = () => {
+    const {t} = useTranslation();
     return (
-        <div>
-           404
-        </div>
+        <>
+            <FlexContainer justifyContent="center" alignItems="center" flexDirection="column">
+                <ErrorPageNumber>404</ErrorPageNumber>
+                <ErrorPageText>{t('Page Not Found')}</ErrorPageText>
+                <Button width="auto" margin="10px">{t('Go To the main page')}</Button>
+            </FlexContainer>
+        </>
     );
 };
 
